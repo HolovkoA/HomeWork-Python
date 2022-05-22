77ea4aad034cce8d32399fde8812966220e6db81
Aleksandr <aleksandr.golovko23@gmail.com>
2022-05-22 16:33:03 +0300

    While

---
+++ main.py
@@ -0,0 +1,23 @@
+def вайл()
+    a = int(input("введіть a:" ))
+    a = + 5
+    print(a)
+    b = int(input("введіть b:" ))
+    c = int(input("введіть c:" ))
+    if a==b:
+        print("Рівність")
+    elif a<b:
+        print("Щось сумне")
+    if a==b:
+        print("Рівність")
+    elif a<b:
+        print("Щось сумне")
+    elif a>b:
+        print("Радісне повідомлення")
+    a=3
+    while a<=10:
+        if a != 5:
+            print(a)
+            a = a + 5
+            continue
+вайл()
