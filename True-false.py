022d8107062befac37ee5315f7fdfb9e39f4679e
Aleksandr <aleksandr.golovko23@gmail.com>
2022-05-22 17:08:16 +0300

    True-False

---
+++ True-false.py
@@ -0,0 +1,13 @@
+def numbers():
+    a = input("введіть a:")
+    b = input("введіть b:")
+    c = input("введіть c:")
+    if a>b:
+        print("дуже сумно")
+    elif a==b:
+        print("рівність")
+    elif b>c:
+        print("радісне повідомлення")
+    else:
+        print("не дуже сумне повідолмення")
+numbers()
